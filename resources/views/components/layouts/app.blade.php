cea23dc312374c88592ff790de60100a180b9894
ChelseaVPutri <chelseavp2706@gmail.com>
2025-12-03 21:22:08 +0700

    update app blade

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+    <title>{{ $title ?? 'Page Title' }}</title>
+    <script src="https://kit.fontawesome.com/803b706a8d.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    @livewireStyles
+</head>
+
+<body class="min-h-screen relative flex flex-col">
+    <div>
+        {{ $slot }}
+    </div>    
+    @livewireScripts
+</body>
+
+</html>
