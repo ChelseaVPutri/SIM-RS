640f4b997fe127a604af241aaa658d5732f5f4bf
ChelseaVPutri <chelseavp2706@gmail.com>
2025-12-05 01:40:59 +0700

    update navbar

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -1,10 +1,13 @@
-<nav class="bg-linear-to-r from-[#26619C] to-[#2A9D8F] w-full sticky top-0 shadow-md">
-    <div class="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+<nav class="bg-linear-to-r from-primary-blue to-primary-green w-full sticky top-0 shadow-md">
+    <div class="flex h-16 items-center justify-between px-4 sm:px-6 lg:px-8">
         {{-- Nama Aplikasi --}}
-        <div class="flex items-center gap-4">
-            <div class="flex h-10 w-10 items-center">
-                <h1 class="font-display text-xl font-bold text-white">SI-SHIFTRS</h1>
+        <div class="flex items-center gap-3">
+            <div class="flex h-10 w-10 items-center justify-center bg-white/20 rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" class="text-white">
+                    <path fill="currentColor" d="M10.5 17h3v-3.5H17v-3h-3.5V7h-3v3.5H7v3h3.5zM5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.587 1.413T19 21zm0-2h14V5H5zM5 5v14z"/>
+                </svg>
             </div>
+            <h1 class="font-display text-xl font-bold text-white">MediShift</h1>
         </div>
 
         {{-- Button --}}
