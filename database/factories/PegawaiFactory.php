4a9886ec16cf2eadc9543f3f72130d068f338cfb
ChelseaVPutri <chelseavp2706@gmail.com>
2025-12-06 18:13:25 +0700

    add factory pegawai

---
+++ database/factories/PegawaiFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Department;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Pegawai>
+ */
+class PegawaiFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        return [
+            'nama_lengkap' => $this->faker->name(),
+            'nip' => str_pad($this->faker->numberBetween(1, 99999), 5, '0', STR_PAD_LEFT),
+            'department_id' => Department::inRandomOrder()->first()->id,
+            'sisa_cuti' => 12,
+            'password' => Hash::make('123'),
+            'role' => 'user'
+        ];
+    }
+
+    public function manajer() {
+        return $this->state(function (array $attributes) {
+            return [
+                'role' => 'manajer'
+            ];
+        }); 
+    }
+}
